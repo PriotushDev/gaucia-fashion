48030c83d381f4ffe0e0360537bc4ccdd5666d67
Priyotosh <ai.priyobabu@gmail.com>
2026-02-03 18:42:19 +0600

    website master-page add

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\WebsiteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WebsiteController::class, 'index'])->name('website.home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+
+    // admin panel route
 });
